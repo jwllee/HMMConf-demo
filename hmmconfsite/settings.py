077ca9fd13f64261ed7ccbd530d49f65e3f879ff
Wai Lam Jonathan LEE <jonathan.wailam.lee@gmail.com>
2019-11-19 22:31:21 -0300

    Hard code allowed hosts

--- hmmconfsite/settings.py
+++ hmmconfsite/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = [
+    'hmmconf.herokuapp.com',
+    '127.0.0.1',
+    'localhost',
+]
 
 # Application definition
 
